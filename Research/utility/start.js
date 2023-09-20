1e8244c08ca67f034a31b147a38920edc4ef08be
Daniele Bosc <danielebosc@gmail.com>
2023-09-20 19:40:58 +0200

    some articles have been added

--- Research/utility/start.js
+++ Research/utility/start.js
@@ -1,5 +1,6 @@
 import open from "open";
 
+open("https://web.whatsapp.com/");
 open("https://mail.google.com/mail/u/1/#inbox");
 open("https://github.com/BoscDaniele/Tesi");
 open("https://researchrabbitapp.com/home");

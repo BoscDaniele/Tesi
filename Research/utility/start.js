0987dea11d6631a8616ae9e9b846556236b32e51
Daniele Bosc <danielebosc@gmail.com>
2023-09-20 11:19:14 +0200

    some changes on start.js

--- Research/utility/start.js
+++ Research/utility/start.js
@@ -1,6 +1,5 @@
 import open from "open";
 
-open("https://web.whatsapp.com");
 open("https://mail.google.com/mail/u/1/#inbox");
 open("https://github.com/BoscDaniele/Tesi");
 open("https://researchrabbitapp.com/home");
